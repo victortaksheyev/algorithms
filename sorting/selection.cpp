0d1090999569df0dd31955b977ca8992bf7742b9
victortaksheyev <victortaksheyev@gmail.com>
2019-02-02 22:19:37 -0800

    upload selection.cpp

---
+++ sorting/selection.cpp
@@ -0,0 +1,34 @@
+// selection sort
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int nums[1000];
+    int length = 0;
+    
+    while (cin >> nums[length])
+        length++;
+
+    int smallestI;
+    
+    for (int i = 0; i < length; i++) {
+        smallestI = i;
+        for (int j = i+1; j < length; j++) {
+            if (nums[j] < nums[smallestI]) {
+                smallestI = j;
+            }
+        }
+        int temp;
+        temp = nums[smallestI];
+        nums[smallestI] = nums[i];
+        nums[i] = temp;
+    }
+    
+    for (int i = 0; i < length; i++) {
+        cout << nums[i] << endl;
+    }
+    
+    return 0;
+}
